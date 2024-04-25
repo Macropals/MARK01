2cc0693ac21aa8f74c3f971e10f84a4cc7b53d35
Articha <tima-1324@mail.ru>
2024-04-25 21:52:45 +0300

    Init: • Added basic Django project • Added pyproject.toml with all necessary information • Moved Django SECRET_KEY to environment variable

---
+++ src/ecology/__main__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main(arguments: str):
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangosite.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(arguments)
+
+
+def startapp():
+    main(['manage.py', 'runserver', '--noreload'])
+
+
+if __name__ == "__main__":
+    startapp()
+    # main(sys.argv)
