1891a988cd678abdf12fdf1240258158766f0bf5
ArtichaTM <tima-1324@mail.ru>
2024-04-27 13:15:29 +0300

    Added models in hackaton Changed test temp.py Added new words for cSpell

--- src/ecology/hackaton/models.py
+++ src/ecology/hackaton/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Floor(models.Model):
+    index = models.IntegerField(primary_key=True, null=False)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    center_x = models.FloatField(blank=True, null=False, default=0)
+    center_y = models.FloatField(blank=True, null=False, default=0)
+
+class Device(models.Model):
+    floor = models.ForeignKey('Floor', on_delete=models.RESTRICT)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    x = models.FloatField(blank=True, null=True, default=0)
+    y = models.FloatField(blank=True, null=True, default=0)
+    x_extents = models.FloatField(blank=True, null=True, default=2)
+    y_extents = models.FloatField(blank=True, null=True, default=1)
+
+class DeviceData(models.Model):
+    device = models.ForeignKey('Device', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True, editable=False)
+    data = models.JSONField()
+
+class FloorRectangle(models.Model):
+    floor = models.ForeignKey('Floor', on_delete=models.CASCADE)
+    x = models.FloatField(blank=True, null=True, default=0)
+    y = models.FloatField(blank=True, null=True, default=0)
+    x_extents = models.FloatField(blank=True, null=True, default=2)
+    y_extents = models.FloatField(blank=True, null=True, default=1)
+    text = models.CharField(max_length=50, blank=False, null=False)
--- tests/temp.py
+++ tests/temp.py
@@ -1,18 +1,11 @@
 import unittest
 import requests
-import responses
 
 class TestCase(unittest.TestCase):
 
-  def testExample(self):
-    # responses.add(**{
-    #   'method'         : responses.PUT,
-    #   'url'            : 'http://127.0.0.1:800',
-    #   'body'           : '{"error": "reason"}',
-    #   'status'         : 404,
-    #   'content_type'   : 'application/json',
-    #   'adding_headers' : {'X-Foo': 'Bar'}
-    # })
+  def testUpload(self):
     response = requests.put('http://127.0.0.1:8000/add-info', json={'info': 12})
-    print(response)
     self.assertEqual(200, response.status_code)
+    json = response.json()
+    self.assertTrue('message' in json)
+    self.assertEqual(json['message'], 'OK')
