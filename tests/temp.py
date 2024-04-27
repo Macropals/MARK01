a8b95aba86dd32b5cb7cc09bd0f6300cca3729ea
ArtichaTM <tima-1324@mail.ru>
2024-04-27 14:56:00 +0300

    Reformatted temp test

--- tests/temp.py
+++ tests/temp.py
@@ -2,28 +2,28 @@ import unittest
 import requests
 
 class TestCase(unittest.TestCase):
-  HOST = 'http://127.0.0.1:8000'
+    HOST = 'http://127.0.0.1:8000'
 
-  def testUpload(self):
-    self.skipTest('Deprecated')
-    response = requests.put(f'{self.HOST}/add-info', json={'info': 12})
-    self.assertEqual(200, response.status_code)
-    json = response.json()
-    self.assertTrue('message' in json)
-    self.assertEqual(json['message'], 'OK')
+    def testUpload(self):
+        self.skipTest('Deprecated')
+        response = requests.put(f'{self.HOST}/add-info', json={'info': 12})
+        self.assertEqual(200, response.status_code)
+        json = response.json()
+        self.assertTrue('message' in json)
+        self.assertEqual(json['message'], 'OK')
 
-  def testLatestDevice(self):
-    response = requests.get(f'{self.HOST}/get/latest_device', json={'id': 1})
-    self.assertEqual(200, response.status_code)
-    json = response.json()
-    self.assertTrue('message' in json)
-    self.assertEqual(json['message'], 'OK', json)
-    self.assertTrue('data' in json)
+    def testLatestDevice(self):
+        response = requests.get(f'{self.HOST}/get/latest_device', json={'id': 1})
+        self.assertEqual(200, response.status_code)
+        json = response.json()
+        self.assertTrue('message' in json)
+        self.assertEqual(json['message'], 'OK', json)
+        self.assertTrue('data' in json)
 
-  def testAllDevices(self):
-    response = requests.get(f'{self.HOST}/get/device')
-    self.assertEqual(200, response.status_code)
-    json = response.json()
-    self.assertTrue('message' in json)
-    self.assertEqual(json['message'], 'OK', json)
-    print(json['devices'])
+    def testAllDevices(self):
+        response = requests.get(f'{self.HOST}/get/device')
+        self.assertEqual(200, response.status_code)
+        json = response.json()
+        self.assertTrue('message' in json)
+        self.assertEqual(json['message'], 'OK', json)
+        print(json['devices'])
