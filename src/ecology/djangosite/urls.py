4972aed2590d9316c1a2a3601e9d5f790fd9530a
ArtichaTM <tima-1324@mail.ru>
2024-04-27 17:14:04 +0300

    Added redirect from '' to 'api/'

--- src/ecology/djangosite/urls.py
+++ src/ecology/djangosite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/', include('hackaton.urls'))
+    path('api/', include('hackaton.urls')),
+    path('', RedirectView.as_view(pattern_name='hackaton:index', permanent=False))
 ]
--- src/ecology/hackaton/urls.py
+++ src/ecology/hackaton/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
 from .views import *
+from .apps import HackatonConfig
+
+app_name = HackatonConfig.name
 
 urlpatterns = [
     path("", general.index, name='index'),
