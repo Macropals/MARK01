ad01406d8c8c32feaa7161b6458c0f284cad5bbd
Articha <tima-1324@mail.ru>
2024-04-28 12:46:22 +0300

    Added adding views

--- src/ecology/hackaton/views/add.py
+++ src/ecology/hackaton/views/add.py
@@ -1,25 +1,76 @@
-from django.http.response import Http404
+from json import loads, JSONDecodeError
 
+from django.http.response import Http404, JsonResponse, HttpResponseBadRequest
+
+from ..models import Device, Floor, DeviceData
 
 def device_data(request):
-    raise Http404('Not implemented')
-    # if request.method == 'PUT':
-    #     try:
-    #         data = loads(request.body)
-    #     except JSONDecodeError as e:
-    #         return JsonResponse({"message": "error", "error": e.args})
-    # else:
-    #     return HttpResponseBadRequest("This URL required PUT HTTP method")
-    # return JsonResponse({"message": 'NotImplemented'})
+    if request.method == 'PUT':
+        try:
+            data = loads(request.body)
+        except JSONDecodeError as e:
+            return JsonResponse({"message": "error", "error": e.args})
+    else:
+        return HttpResponseBadRequest("This URL required PUT HTTP method")
+    try:
+        data = DeviceData(**data)
+    except Exception as e:
+        return JsonResponse({
+            "message": 'error',
+            "error": e.args[0]
+        })
+    return JsonResponse({"message": 'NotImplemented'})
 
 
 def floor(request):
-    raise Http404('Not implemented')
+    if request.method == 'PUT':
+        try:
+            data = loads(request.body)
+        except JSONDecodeError as e:
+            return JsonResponse({"message": "error", "error": e.args})
+    else:
+        return HttpResponseBadRequest("This URL required PUT HTTP method")
+    try:
+        data = Floor(**data)
+    except Exception as e:
+        return JsonResponse({
+            "message": 'error',
+            "error": e.args[0]
+        })
+    return JsonResponse({"message": 'NotImplemented'})
 
 
 def device(request):
-    raise Http404('Not implemented')
+    if request.method == 'PUT':
+        try:
+            data = loads(request.body)
+        except JSONDecodeError as e:
+            return JsonResponse({"message": "error", "error": e.args})
+    else:
+        return HttpResponseBadRequest("This URL required PUT HTTP method")
+    try:
+        data = Floor(**data)
+    except Exception as e:
+        return JsonResponse({
+            "message": 'error',
+            "error": e.args[0]
+        })
+    return JsonResponse({"message": 'NotImplemented'})
 
 
 def floor_rectangle(request):
-    raise Http404('Not implemented')
+    if request.method == 'PUT':
+        try:
+            data = loads(request.body)
+        except JSONDecodeError as e:
+            return JsonResponse({"message": "error", "error": e.args})
+    else:
+        return HttpResponseBadRequest("This URL required PUT HTTP method")
+    try:
+        data = Floor(**data)
+    except Exception as e:
+        return JsonResponse({
+            "message": 'error',
+            "error": e.args[0]
+        })
+    return JsonResponse({"message": 'NotImplemented'})
